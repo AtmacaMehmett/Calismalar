cc6ca9131e0019ba20f94fba175777aabd37fd0a
AtmacaMehmett <mehmettopak453@gmail.com>
2025-03-01 16:53:35 +0300

    Initial commit

---
+++ Core/Inc/keypad.h
@@ -0,0 +1,41 @@
+#ifndef KEYPAD_H
+#define KEYPAD_H
+
+#include "stm32f4xx_hal.h"
+
+
+
+// Fonksiyon Prototipi
+char Read_Keypad(void);
+
+#endif  // KEYPAD_H
+
+
+
+
+
+///*
+// * keypad.h
+// *
+// *  Created on: Feb 25, 2025
+// *      Author: Yasin Batuhan URAL
+// */
+//
+//#ifndef keyPad_H
+//#define keyPad_H
+//
+//#include "main.h"
+//#include "macro.h"
+//
+//
+//#define FACTORY_RESET 578
+//
+//
+//
+//
+//#define KEYPAD_D   32589
+//// Keypad fonksiyonları
+//char Read_Keypad(void);
+//
+//
+//#endif
